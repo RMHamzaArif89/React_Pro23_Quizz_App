d2dc0f899738925a00105ec0d4b089531dcf07ff
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-05-20 08:39:10 +0500

    created the quizz app components

---
+++ Quizz_App/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './css/header.css'
+
+function Header() {
+    let[state,setState]=useState(true)
+
+    return (
+        <div className='header'>
+            <div className='heading'>Quizz App</div>
+            {
+                state?
+                <h1 onClick={()=>{setState(!state)}}>Start the quizz</h1>:
+                <h2>Choose the Coorect Answer</h2>
+
+            }
+         
+            <iframe src="https://giphy.com/embed/8zjxfpuutpJFRnlM2h"
+             width="200" height="120" frameBorder="0" 
+             class="giphy-embed" allowFullScreen></iframe><p>
+           </p>
+
+
+        </div>
+    )
+}
+
+export default Header
